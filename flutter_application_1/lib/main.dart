108abbc38847ce7485c03b2b1d80aba367d82f7b
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-05 11:00:56 +0700

    create new app to get user input

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_application_1/week04/counter.dart';
+import 'package:flutter_application_1/week04/input_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Counter()));
+  runApp(MaterialApp(home: InputDemo()));
 }
