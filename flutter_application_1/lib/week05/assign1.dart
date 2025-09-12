7becaf3d22be2d148a2e4e50f8433c85c216e862
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-12 23:01:29 +0700

    finished assignment 1  wedding

--- flutter_application_1/lib/week05/assign1.dart
+++ flutter_application_1/lib/week05/assign1.dart
@@ -8,13 +8,63 @@ class Assign1 extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.pink[200],
 
-      body: Column(
-        children: [
-          const Text(
-            'Wedding Organizer',
-            style: TextStyle(color: Colors.white, fontFamily: "Sevillana"),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(),
+            Center(
+              child: Column(
+                children: [
+                  const Text(
+                    'Wedding Organizer',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: "Sevillana",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  const Text(
+                    'Pre-wedding. Photo, Party',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontFamily: "Sevillana",
+                    ),
+                  ),
+
+                  SizedBox(height: 16),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text(
+                      'Our Services',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                '345 Moo 1 Thasud Chiang Rai, Thailand',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
