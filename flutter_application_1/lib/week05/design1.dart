db2036665913083fe397aa8c4008cdb9c1fcdd75
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-12 10:50:36 +0700

    finished design 1

--- flutter_application_1/lib/week05/design1.dart
+++ flutter_application_1/lib/week05/design1.dart
@@ -5,6 +5,26 @@ class Design1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(width: 100, color: Colors.red),
+          Expanded(
+            child: Container(
+              width: 100,
+              color: Colors.teal,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(width: 100, height: 100, color: Colors.yellow),
+                  Container(width: 100, height: 100, color: Colors.green),
+                ],
+              ),
+            ),
+          ),
+          Container(width: 100, color: Colors.blue),
+        ],
+      ),
+    );
   }
 }
