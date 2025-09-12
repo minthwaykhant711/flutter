8887e0a15d58941d36627513f8a55698826292fb
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-12 10:00:49 +0700

    doing single child demo

---
+++ flutter_application_1/lib/week05/single_child_demo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SingleChildDemo extends StatelessWidget {
+  const SingleChildDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(title: Text('My app')),
+      body: SafeArea(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.yellow,
+          padding: EdgeInsets.all(16),
+          margin: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          child: Text('Single child'),
+        ),
+      ),
+    );
+  }
+}
