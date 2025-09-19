523d36fc7a054cbf4ededf6b08295f1c8fa50108
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-19 11:10:56 +0700

    login dynamic bg colors design

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/week06/baking_app.dart';
+
+import 'package:flutter_application_1/week06/login_dynamic.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: BakingApp()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginDynamic()));
 }
---
+++ flutter_application_1/lib/week06/login_dynamic.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class LoginDynamic extends StatefulWidget {
+  const LoginDynamic({super.key});
+
+  @override
+  State<LoginDynamic> createState() => _LoginDynamicState();
+}
+
+class _LoginDynamicState extends State<LoginDynamic> {
+  List<Color> darkBgColors = [
+    const Color(0xFF0D1441),
+    const Color(0xFF283584),
+    const Color(0xFF376AB2),
+  ];
+
+  List<Color> lightBgColors = [
+    const Color(0xFF8C2480),
+    const Color(0xFFCE587D),
+    const Color(0xFFFF9485),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    DateTime now = DateTime.now();
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: now.hour < 18 ? lightBgColors : darkBgColors,
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(),
+                    Text(
+                      now.hour < 18 ? 'Good Morning' : 'Good Evening',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                    Text(
+                      'Please sign in below',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Email',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                    TextField(),
+                    SizedBox(height: 15),
+                    Text(
+                      'Password',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                    TextField(),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Image.asset(
+                'assets/images/landscape.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
