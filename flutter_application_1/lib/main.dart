1140b0d6cd11096bcb4b9d39830999fd8b0be676
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-09-23 23:55:21 +0700

    nothing special

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_application_1/week06/week6_ex5.dart';
+import 'package:flutter_application_1/week06/assign_2.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Week6Ex5()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Assign2()));
 }
