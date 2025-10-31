417bd965c421389e48710fce210149047b318ca8
minthwaykhant711 <6631501176@lamduan.mfu.ac.th>
2025-10-31 10:02:13 +0700

    finished

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/expenses/login.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App', style: TextStyle(color: Colors.white)),
-          backgroundColor: Color.fromARGB(255, 0, 0, 255),
-        ),
-
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 22, color: Colors.blue[700]),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          backgroundColor: Colors.blue,
-          // child: Text('Click'),
-          child: Icon(Icons.add, color: Colors.white),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Login()));
 }
